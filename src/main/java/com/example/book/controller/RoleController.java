8028cb83261baf306eb8fcc019434de7a636f150
jimtm23 <jimmartinez23@gmail.com>
2021-02-27 21:58:51 +0800

    fixe role

--- src/main/java/com/example/book/controller/RoleController.java
+++ src/main/java/com/example/book/controller/RoleController.java
@@ -6,9 +6,7 @@ import com.example.book.service.RoleService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 
@@ -16,38 +14,38 @@ import java.net.URI;
 @AllArgsConstructor
 @RestController
 @RequestMapping("role")
-public class RoleController implements CrudController<Role> {
+public class RoleController {
 
     RoleService roleService;
 
     @PreAuthorize("hasRole('ADMIN')")
-    @Override
+    @GetMapping("/show/{id}")
     public ResponseEntity<Role> show(String id) {
         return ResponseEntity.ok(roleService.findById(id));
     }
 
     @PreAuthorize("hasRole('ADMIN')")
-    @Override
+    @GetMapping("/list")
     public ResponseEntity<Iterable<Role>> list() {
         return ResponseEntity.ok(roleService.list());
     }
 
     @PreAuthorize("hasRole('ADMIN')")
-    @Override
+    @PostMapping("/create")
     public ResponseEntity<Role> create(Role role) {
         Role newRole = roleService.save(role);
         return ResponseEntity.created(URI.create("/role/show/" + newRole.getId())).build();
     }
 
     @PreAuthorize("hasRole('ADMIN')")
-    @Override
+    @PutMapping("/edit/{id}")
     public ResponseEntity<Role> edit(String id, Role role) {
         Role thisRole = roleService.edit(id, role);
         return ResponseEntity.created(URI.create("/role/show/" + thisRole.getId())).build();
     }
 
     @PreAuthorize("hasRole('ADMIN')")
-    @Override
+    @DeleteMapping("/delete/{id}")
     public ResponseEntity<Boolean> delete(String id) {
         roleService.delete(id);
         return ResponseEntity.ok(true);
